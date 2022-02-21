b6fe264b78e53a9dbdef71fc2bf17939f39bc6e3
OmkarBichukale10 <omkarbichukale777@gmail.com>
2022-02-21 18:40:18 +0530

    modified bt Omkar

---
+++ server.js
@@ -0,0 +1,18 @@
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Created By :Omkar Bichukale" );
+};
+var onDefault=function(req, res){
+    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+              "<hr/>"+
+              "<ol>"+
+                    " <li>Contosa is a multinational company.</li>"+
+                    " <li>With 12 offices world wide.</li>"+
+             "</ol>");
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8081);
+console.log("Server is running on port 8081");
